9bca5ccf55d01679e5ca454c5d158bd864810545
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-01-22 12:28:01 +0600

    final

--- assignment.js
+++ assignment.js
@@ -1,20 +1,26 @@
+//   https://github.com/Tanjin9185/Assignment3/blob/main/assignment.js
+
+
+// 1. kilometerToMeter
 function kilometerToMeter(kiloMeter){
   if(kiloMeter<0){
       var meter = "Kilometer Can not be Negetive Digit!!";
       return meter;
   }
   else{
-      meter = kiloMeter*1000; //we know, 1k.m = 1000m
+      meter = kiloMeter*1000;                 //we know, 1k.m = 1000m
       return meter;   
   }
 }
-var Meter = kilometerToMeter(11); //input kilometer
+var Meter = kilometerToMeter(11);             //input kilometer
 console.log("Total meter is :",Meter);
 
+
+// 2. budgetCalculator
 function budgetCalculator(watch, phone, laptop){
   if(watch < 0){
-    console.log("Watch Quantity Can not be Negetive Digit!!"); // check quantity(can not be -1<=) 
-    watch = 0; // negetive quantity return 0 quantity
+    console.log("Watch Quantity Can not be Negetive Digit!!");       // check quantity(can not be -1<=) 
+    watch = 0;                                                       // negetive quantity return 0 quantity
   }
   if(phone < 0){
     console.log("Phone Quantity Can not be Negetive Digit!!");
@@ -24,14 +30,14 @@ function budgetCalculator(watch, phone, laptop){
     console.log("Laptop Quantity Can not be Negetive Digit!!");
     laptop = 0;
   }
-  return watch*50+phone*100+laptop*500; //return total budget amount.
+  return watch*50+phone*100+laptop*500;                              //return total budget amount.
 }
-var result = budgetCalculator(1,1,-1); //budgetCalculator(watch quantity, phone quantity, laptop quantity)
+var result = budgetCalculator(1,1,-1);                               //budgetCalculator(watch quantity, phone quantity, laptop quantity)
 console.log("Total budget is :",result); 
 
 
-var amount = 0; //initially total amount = 0
-
+// 3. hotelCost
+var amount = 0;                                                     //initially total amount = 0
 function hotelCost(day){
     if(day<0){
         amount = "Day Can not be Negetive Digit!!";
@@ -61,8 +67,9 @@ var totalCost = hotelCost(127);          //hotelCost(total days)
 console.log("Total cost is :",totalCost); 
 
 
+// 4. megaFriend
 function megaFriend(arr){
-  var len = 0;  // initially declar array lengtgh(len) is 0 
+  var len = 0;                             // initially declar array lengtgh(len) is 0 
   var longest;
   if(arr.length<1){
       longest = "Your Array is empty";
@@ -80,5 +87,4 @@ function megaFriend(arr){
 }
 var arr = ["Jhanker Mahbub", "asdfgh asdfghj", "Foysal", "Manik Roy", "Anik", "Najmus Sakib"];
 var result = megaFriend(arr);
-
 console.log("Max string name is :",result);
